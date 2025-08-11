6e76d19a4b6193a8ec6e892344c01957190d434b
chadongmin <cdm2883@naver.com>
2025-08-11 23:36:02 +0900

    initial commit

---
+++ src/main/java/com/trevari/TrevariApplication.java
@@ -0,0 +1,13 @@
+package com.trevari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrevariApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrevariApplication.class, args);
+    }
+
+}
